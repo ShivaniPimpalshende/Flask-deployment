f81385fa6673d097fe530f61f63dee80c31c986f
Shivani Pimpalshende <shivanipimpalshende02@gmail.com>
2024-04-18 15:59:40 +0530

    first Deployment

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import pandas as pd
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-app = Flask(__name__)
+app = Flask(__name__,static_url_path='/static')
 
 # Assuming you have a dataset named 'flipkart.csv' with columns 'Product_name' and 'Review'
 df = pd.read_csv('flipkart.csv')
@@ -11,23 +11,29 @@ sid = SentimentIntensityAnalyzer()
 def analyze_sentiment(review):
     sentiment_score = sid.polarity_scores(review)['compound']
     if sentiment_score > 0.05:
-        return 'Positive 🙂'
+        return 'Positive reviews 🙂'
     elif sentiment_score < -0.05:
-        return 'Negative 😞 '
+        return 'Negative reviews😞 '
     else:
-        return 'Neutral  😐'
+        return 'Neutral reviews 😐'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+     return render_template('index.html')
+   
 @app.route('/analyze', methods=['POST'])
 def analyze():
     product_name = request.form['productName']
     product_reviews = df[df['Product_name'] == product_name]['Review']
     sentiments = product_reviews.apply(analyze_sentiment)
     sentiment_counts = sentiments.value_counts().to_dict()
+    
     return jsonify(sentiment_counts)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
